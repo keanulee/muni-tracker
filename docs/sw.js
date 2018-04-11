134a5e4d8c5fd82b7605aed7928db31a0d78f7cb
Keanu Lee <keanulee517@gmail.com>
2018-04-10 21:10:42 -0700

    Generate SW with Workbox

--- docs/sw.js
+++ docs/sw.js
@@ -1,8 +1,36 @@
-// https://jakearchibald.com/2014/offline-cookbook/#network-falling-back-to-cache
-self.addEventListener('fetch', function(event) {
-  event.respondWith(
-    fetch(event.request).catch(function() {
-      return caches.match(event.request);
-    })
-  );
-});
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.0.1/workbox-sw.js");
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [
+  {
+    "url": "index.html",
+    "revision": "4093baaa3d88375f5cb9506d1767e67e"
+  },
+  {
+    "url": "script.js",
+    "revision": "5d55a1e6fa8f30e41b8d6f8dc88de9c6"
+  },
+  {
+    "url": "style.css",
+    "revision": "32357df28c91e79ffb5106889d9d8f4d"
+  }
+].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
---
+++ workbox-config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  "globDirectory": "docs/",
+  "globPatterns": [
+    "index.html",
+    "script.js",
+    "style.css"
+  ],
+  "swDest": "docs/sw.js"
+};
